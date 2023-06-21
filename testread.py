82a33003accc65d6332169502c1335410b183b09
Diego Hernandez <dmonsivais173@gmail.com>
2023-06-21 13:43:34 -0400

    got classrooms and times being used into a dict

--- testread.py
+++ testread.py
@@ -23,15 +23,53 @@ def main(argv):
 
     #This line keeps all rows that contain 'Kohlberg' in its 'BLDG_RM1' col 
     notValidRooms = ["Lang Perf Arts Ctr", "Matchbox", "Off Campus", "TBA", "Whittier", "Lang Music", 
-        "Ware", "Fieldhouse", "Mullan Tennis", "Lodges"]
+        "Ware", "Fieldhouse", "Mullan Tennis", "Lodges", "Tarble GYM", "Wister Center"]
     df = df[~(df['BLDG_RM1'].str.contains('|'.join(notValidRooms)))]
     #Reset the table indices
     df = df.reset_index()
-    print(df)
+    #print(df)
     print(df.shape)
 
-    finaldf = pd.DataFrame(0, index = df['BLDG_RM1'].unique(), columns = list("MTWTF")) 
+
+    #where 1 is the day, 2 is the time, 3 is the room
+    #rowz = df.iloc[0, 1]
+    #print(rowz)
+    
+    roomDict = {}
+    #dfSize = df.shape
+    #print(dfSize[0])
+    #print(df.iloc[0, 3])
+    count = 0
+    for i in range (df.shape[0]):
+        #if room already in dic
+        if df.iloc[i, 3] in roomDict:
+            #get the list, append new item, update val in dic
+            #print("a")
+            currentTimes = roomDict.get(df.iloc[i, 3])
+            currentTimes.append((df.iloc[i, 1], df.iloc[i, 2]))
+            roomDict.update({df.iloc[i, 3]: currentTimes})
+            count += 1
+        #else if room not in dict
+        else:
+            #add room to dic with list of times
+            #print("b")
+            roomDict[df.iloc[i, 3]] = [(df.iloc[i, 1], df.iloc[i, 2])]
+            count += 1
+
+
+    print("The loop worked?")
+    print("count is %d" % (count))
+    #print(roomDict.keys())
+    #for i in roomDict.keys():
+     #   print(i)
+    
+    for i in roomDict.get("Science Center 181"):
+        print(i)
+    
+    finaldf = pd.DataFrame(0, index = df['BLDG_RM1'].unique(), columns = list("MTWHF")) 
     #print(finaldf)
+
+
     return
 
 if __name__ == "__main__":
